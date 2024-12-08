e7002e76244cce0c2deb17de8c917f2e89f2dc28
Nguyen Hien <nguyenhien.bds.com@gmail.com>
2024-12-08 23:03:39 +0700

    BE: Create SecurityConfig

--- src/server/src/main/java/com/hiennguyen/hotelapp/configuration/SecurityConfiguration.java
+++ src/server/src/main/java/com/hiennguyen/hotelapp/configuration/SecurityConfiguration.java
@@ -1,16 +1,44 @@
 package com.hiennguyen.hotelapp.configuration;
 
+import java.util.Collections;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
 
 @Configuration
 @EnableMethodSecurity
 public class SecurityConfiguration {
-        @Bean
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public CorsFilter corsFilter() {
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        CorsConfiguration config = new CorsConfiguration();
+
+        config.setAllowCredentials(true);
+        // Allow 4200 and 3000 ports for development
+        config.setAllowedOrigins(Collections.singletonList("http://localhost:4200"));
+        config.addAllowedHeader("*"); // X-Requested-With, Content-Type, Authorization, Origin, Accept
+        config.addAllowedMethod("*"); // GET, POST, PUT, DELETE, PATCH, OPTIONS
+        source.registerCorsConfiguration("/**", config);
+
+        return new CorsFilter(source);
+    }
+    
+    @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http.csrf(csrf -> csrf.disable())
                 .authorizeHttpRequests(authorizeRequests -> authorizeRequests
