c7da2c3758de71ef57958fa60c1f1b83f391d907
Nguyen Hien <nguyenhien.bds.com@gmail.com>
2024-12-08 11:18:57 +0700

    FE: Create Roomlist and RoomDetail component

--- src/client/src/modules/manager/room/room-details/room-details.component.ts
+++ src/client/src/modules/manager/room/room-details/room-details.component.ts
@@ -1,12 +1,99 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { HttpClient } from '@angular/common/http';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { FormGroup, FormControl, Validators, ReactiveFormsModule } from '@angular/forms';
+import { FontAwesomeModule, IconDefinition } from '@fortawesome/angular-fontawesome';
+import { faCancel, faSave, faRotateLeft } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-room-details',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule, FontAwesomeModule, CommonModule],
   templateUrl: './room-details.component.html',
   styleUrl: './room-details.component.css'
 })
 export class RoomDetailsComponent {
+  private _selectedItem!: any;
 
+  @Input('selected-item') set selectedItem(value: any) {
+    if (value != null) {
+      this._selectedItem = value;
+      this.isEdit = true;
+      this.updatedForm();
+    } else {
+      if (this.form) {
+        this.form.reset();
+      }
+      this.isEdit = false;
+    }
+  }
+
+  get selectedItem(): any {
+    return this._selectedItem;
+  }
+
+  @Output() cancel: EventEmitter<void> = new EventEmitter<void>();
+
+  public isEdit: boolean = false;
+  private apiUrl: string = 'http://localhost:8080/api/v1/rooms';
+  public form!: FormGroup;
+
+  public faCancel: IconDefinition = faCancel;
+  public faSave: IconDefinition = faSave;
+  public faRotateLeft: IconDefinition = faRotateLeft;
+
+  constructor(private httpClient: HttpClient) {}
+
+  ngOnInit(): void {
+    this.createForm();
+    this.updatedForm();
+  }
+
+  updatedForm() {
+    if (this.form && this.selectedItem) {
+      this.form.patchValue(this.selectedItem);
+    }
+  }
+
+  private createForm(): void {
+    this.form = new FormGroup({
+      name: new FormControl('', [Validators.required, Validators.minLength(2), Validators.maxLength(255),]),
+      price: new FormControl('', [Validators.required, Validators.min(0)]),
+      active: new FormControl(true),
+    });
+  }
+
+  public onSubmit(): void {
+    //Validate form
+    if(this.form.invalid) {
+      return;
+    }
+
+    // Trich xuat du lieu tuw form
+    const data = this.form.value;
+
+    if(this.isEdit) {
+      this.httpClient.put(`${this.apiUrl}/${this.selectedItem.id}`, data).subscribe((result: any) => {
+        if (result) {
+          console.log('Update seccess');
+          this.cancel.emit();
+        } else {
+          console.log('Update failed');
+        }
+      });
+    } else {
+      // Call API to create new hotel service
+      this.httpClient.post(this.apiUrl, data).subscribe((result: any) => {
+        console.log(result);
+        // Neu thanh cong thi dong form
+        if (result != null) {
+          this.cancel.emit();
+        }
+      });
+    }
+  }
+
+  public onCancel(): void {
+    this.cancel.emit();
+  }
 }
--- src/client/src/modules/manager/room/room-list.component.ts
+++ src/client/src/modules/manager/room/room-list.component.ts
@@ -1,12 +1,84 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, Validators, ReactiveFormsModule } from '@angular/forms';
+import { FontAwesomeModule, IconDefinition } from '@fortawesome/angular-fontawesome';
+import { faPlus, faSearch, faEdit, faTrashCan } from '@fortawesome/free-solid-svg-icons';
+import { RoomDetailsComponent } from './room-details/room-details.component';
 
 @Component({
   selector: 'app-room-list',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, ReactiveFormsModule, FontAwesomeModule, RoomDetailsComponent],
   templateUrl: './room-list.component.html',
   styleUrl: './room-list.component.css'
 })
-export class RoomListComponent {
+export class RoomListComponent implements OnInit{
+  public isShowDetails: boolean = false;
+  public selectedItem!: any;
 
+  public faPlus: IconDefinition = faPlus;
+  public faSearch: IconDefinition = faSearch;
+  public faEdit: IconDefinition = faEdit;
+  public faTrashCan: IconDefinition = faTrashCan;
+
+  public searchForm!: FormGroup;
+  public data: any[] = [];
+
+  private apiUrl: string = "http://localhost:8080/api/v1/rooms/search"
+
+  constructor(private httpClient: HttpClient) {}
+
+  ngOnInit(): void {
+    this.createForm();
+    this.search();
+  }
+
+  private search(): void {
+    this.apiUrl = `http://localhost:8080/api/v1/rooms/search?page=0&size=10`;
+    this.httpClient.get(this.apiUrl).subscribe((data: any) => {
+      this.data = data._embedded.roomDTOList;
+    });
+  }
+
+  private createForm(): void {
+    this.searchForm = new FormGroup({
+      keyword: new FormControl('', [Validators.maxLength(255)])
+    });
+  }
+
+  public onSubmit() {
+    if (this.searchForm.invalid) {
+      return;
+    }
+    this.apiUrl = `http://localhost:8080/api/v1/rooms/searchByRoomNumber?keyword=${this.searchForm.value.keyword}`;
+    this.search();
+  }
+
+  public onDelete(id: string): void {
+    this.httpClient.delete(`${this.apiUrl}/${id}`).subscribe((result: any) => {
+      if (result) {
+        this.search();
+        console.log('Delete success');
+      } else {
+        console.log('Delete failed');
+      }
+    });
+  }
+
+  public onEdit(id: string): void {
+    this.isShowDetails = false;
+    this.selectedItem = this.data.find((item) => item.id === id);
+    this.isShowDetails = true;
+  }
+
+  public onCreate(): void {
+    this.isShowDetails = true;
+    this.selectedItem = null;
+  }
+
+  public cancelDetail(): void {
+    this.isShowDetails = false;
+    this.search();
+  }
 }
